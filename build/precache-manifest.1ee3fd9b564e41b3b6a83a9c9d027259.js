883285cffdeeeca896862aea2eb5a63943cab81f
Jonas Almeida <jo.almeida@reply.com>
2020-08-31 14:15:41 -0300

    adequacoes para deploy no heroku

---
+++ build/precache-manifest.1ee3fd9b564e41b3b6a83a9c9d027259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9d562ebf503c25896d3a86b602a808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29060e3d5a7adc172b00",
+    "url": "/static/css/main.62a933f4.chunk.css"
+  },
+  {
+    "revision": "1d91fe22f6e3e5bfe0f0",
+    "url": "/static/js/2.731bad42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.731bad42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29060e3d5a7adc172b00",
+    "url": "/static/js/main.25fa2b5f.chunk.js"
+  },
+  {
+    "revision": "9e6f520d1f247d6a4610",
+    "url": "/static/js/runtime-main.3d2073c2.js"
+  }
+]);
